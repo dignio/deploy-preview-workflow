59138476fcc7f25a7ee12bb6a8dcd44696992c86
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 08:38:12 +0100

    Wrap the input in quotes and it will not be unbound

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -1,14 +1,14 @@
 set -euxo pipefail
 
 app_name="$1"
-# $2 = app_name_postfix=
+app_name_postfix="$2"
 
 full_app_name="$app_name"
 
 # Prepend the postfix if it exists
-if [ ! -z "$2" ]
+if [ ! -z "app_name_postfix" ]
 then
-    full_app_name+="-$2"
+    full_app_name+="-app_name_postfix"
 fi
 
 echo "::set-output name=full_name::$full_app_name"
