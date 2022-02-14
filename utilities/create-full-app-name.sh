bb648564a9ed80d159df31cde814d889af7bebe6
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 09:01:09 +0100

    Fix my own glitching bugs

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -8,12 +8,12 @@ app_name_postfix="$2"
 full_app_name="$app_name"
 
 # Prepend the postfix if it exists
-if [ ! -z "app_name_postfix" ]
+if [ ! -z "$app_name_postfix" ]
 then
-    full_app_name+="-app_name_postfix"
+    full_app_name+="-$app_name_postfix"
 fi
 
 # Set a limit to 63 characters as k8s has the final say in this
-full_app_name+=$(echo $full_app_name | cut -c -63)
+full_app_name="$(echo $full_app_name | cut -c -63)"
 
 echo "::set-output name=full_name::$full_app_name"
