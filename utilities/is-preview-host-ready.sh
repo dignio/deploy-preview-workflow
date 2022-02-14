2fea7571ab4d8c84033327a3c8077fd5c5f7e88d
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 13:28:59 +0100

    Remove the silent output to prevent exit code 23

--- utilities/is-preview-host-ready.sh
+++ utilities/is-preview-host-ready.sh
@@ -5,9 +5,6 @@ set -exo pipefail
 full_name="$1"
 input_path="$2"
 
-# Sleep to make sure everything is ready before we run the curl command
-sleep 5s
-
 # Run curl 20 times with a retry delay of 15 seconds.
 # For success it will exit with the code 0, and for failure with the code 6.
-curl --retry 20 --retry-delay 15 --no-buffer --silent --output /dev/null "https://$full_name.preview.dignio.dev$input_path"
+curl --retry 20 --retry-delay 15 --no-buffer --head "https://$full_name.preview.dignio.dev$input_path"
