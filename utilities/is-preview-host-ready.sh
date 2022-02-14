9063fa8fe77d4be85cb485e8bda3bf0b52cc906a
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 08:26:53 +0100

    Wait for 5 seconds to make sure everything is ready for the step to run
    This is a test to see if we can prevent error code 23 to happen, because /dev/null does not seem to be available straight away all the time.

--- utilities/is-preview-host-ready.sh
+++ utilities/is-preview-host-ready.sh
@@ -4,6 +4,9 @@ full_name="$1"
 branch_name_kebab_case="$2"
 input_path="$3"
 
+# Sleep to make sure everything is ready before we run the curl command
+sleep 5s
+
 # Run curl 20 times with a retry delay of 15 seconds.
 # For success it will exit with the code 0, and for failure with the code 6.
 curl --retry 20 --retry-delay 15 --no-buffer --silent --output /dev/null "https://$full_name-$branch_name_kebab_case.preview.dignio.dev$input_path"
