2f2bd96b23d812efb785c8cc21f566009013dece
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 08:22:54 +0100

    Write full if statement to make it easier to read

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -6,6 +6,9 @@ app_name_postfix="$2"
 full_app_name="$app_name"
 
 # Prepend the postfix if it exists
-[[ ! -z "$2" ]] && full_app_name+="-$2"
+if [ ! -z "$app_name_postfix" ]
+then
+    full_app_name+="-$app_name_postfix"
+fi
 
 echo "::set-output name=full_name::$full_app_name"
