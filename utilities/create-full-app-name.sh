d263b0ec991cb199bb970948b1d7910476ec3031
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-14 08:32:10 +0100

    Move the second argument directly to the if statement and prepend it if exists

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -1,14 +1,14 @@
 set -euxo pipefail
 
 app_name="$1"
-app_name_postfix="$2" || ""
+# $2 = app_name_postfix=
 
 full_app_name="$app_name"
 
 # Prepend the postfix if it exists
-if [ ! -z "$app_name_postfix" ]
+if [ ! -z "$2" ]
 then
-    full_app_name+="-$app_name_postfix"
+    full_app_name+="-$2"
 fi
 
 echo "::set-output name=full_name::$full_app_name"
