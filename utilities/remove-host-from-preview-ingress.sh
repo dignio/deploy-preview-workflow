61ff035741d6ed2424ceb4b1c22d4023b9b46e64
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-06-17 13:02:16 +0200

    fix: set the correct preview ingress name

--- utilities/remove-host-from-preview-ingress.sh
+++ utilities/remove-host-from-preview-ingress.sh
@@ -6,12 +6,12 @@ namespace="$1"
 full_name="$2"
 
 # Get the index of the host we want to remove from the ingress
-INDEX=$(kubectl get ingress preview-ingress --namespace=$namespace --output=json  | jq '.spec.rules | map(.host == "'"$full_name"'.preview.dignio.dev") | index(true)')
+INDEX=$(kubectl get ingress previews-ingress --namespace=$namespace --output=json  | jq '.spec.rules | map(.host == "'"$full_name"'.preview.dignio.dev") | index(true)')
 
 if [ "$INDEX" != "null" ]
 then
     echo "Deleting host from preview ingress."
 
     # Remove the host from the ingress by using a remove operation
-    kubectl patch ingress preview-ingress --namespace=$namespace --type='json' -p='[ { "op":"remove","path":"/spec/rules/'"$INDEX"'" } ]'
+    kubectl patch ingress previews-ingress --namespace=$namespace --type='json' -p='[ { "op":"remove","path":"/spec/rules/'"$INDEX"'" } ]'
 fi
