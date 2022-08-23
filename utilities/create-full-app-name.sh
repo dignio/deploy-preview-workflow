33ff66cdb70f9e9618212b3d14d16db18093d1f2
ricfort <riccardo@dignio.com>
2022-08-23 11:34:13 +0200

    need a coffee- if else were swapped

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -11,6 +11,8 @@ kebab_branch_name=$4
 
 if [ ! -z "$override_hostname" ]
 then
+    full_app_name="$override_hostname"
+else
     full_app_name="$app_name"
     # Prepend the postfix if it exists
     if [ ! -z "$app_name_postfix" ]
@@ -19,8 +21,6 @@ then
     fi
 
     full_app_name+="-$kebab_branch_name"
-else
-    full_app_name="$override_hostname"
 fi
 # Set a limit to 63 characters as k8s has the final say in this.
 # Remove - if that is the last character
