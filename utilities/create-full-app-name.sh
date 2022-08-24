4d16c8b85c3073f3d2c2a937b738ce89c0d0be3f
ricfort <50705369+ricfort@users.noreply.github.com>
2022-08-24 10:05:42 +0200

    PLAT-741: override hostname in previews (#18)
    * override_hostname
    
    * failed variable name in If statement
    
    * forgot override hostname
    
    * need a coffee- if else were swapped
    
    * upgrade version
    
    * upgrade to v3
    
    * fixing repository checkout
    
    * bumping version
    
    * checking the branch
    
    * addressing correct branch
    
    * rollback
    
    * removed ref to branch before PR

--- utilities/create-full-app-name.sh
+++ utilities/create-full-app-name.sh
@@ -4,18 +4,24 @@ set -euxo pipefail
 
 app_name=$1
 app_name_postfix=$2
-kebab_branch_name=$3
+override_hostname=$3
+kebab_branch_name=$4
 
-full_app_name="$app_name"
 
-# Prepend the postfix if it exists
-if [ ! -z "$app_name_postfix" ]
+
+if [ ! -z "$override_hostname" ]
 then
-    full_app_name+="-$app_name_postfix"
+    full_app_name="$override_hostname"
+else
+    full_app_name="$app_name"
+    # Prepend the postfix if it exists
+    if [ ! -z "$app_name_postfix" ]
+    then
+        full_app_name+="-$app_name_postfix"
+    fi
+
+    full_app_name+="-$kebab_branch_name"
 fi
-
-full_app_name+="-$kebab_branch_name"
-
 # Set a limit to 63 characters as k8s has the final say in this.
 # Remove - if that is the last character
 full_app_name="$(echo $full_app_name | cut -c -63 | sed 's/-$//')"
